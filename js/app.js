373a18a147c52a788617c631dd5bbc74bb7bd594
VitaliiRadaiev <hf.chelenger@gmail.com>
2020-11-20 16:00:23 +0200

    added page

--- js/app.js
+++ js/app.js
@@ -121,7 +121,33 @@ $('img.img-svg').each(function(){
 		}
 	}
 
-// === // Spollers ==================================================================;
+// === // Spollers ==================================================================
+
+
+// === Tabs ==================================================================
+{
+	let tabs = document.querySelector('.tabs');
+	if(tabs) {
+		tabs.querySelectorAll('.tabs__triggers').forEach((item) => {
+			item.addEventListener('click', function(e) {
+				e.preventDefault();
+				const id = e.target.getAttribute('href').replace('#','');
+
+				tabs.querySelectorAll('.tabs__triggers').forEach((child) => {
+					child.classList.remove('active');
+				});
+
+				tabs.querySelectorAll('.tabs__content').forEach((child) => {
+					child.classList.remove('active');
+				});
+
+				item.classList.add('active');
+				document.getElementById(id).classList.add('active');
+			});
+		});
+	}
+}
+// === // Tabs ==================================================================;
 
 
 $(document).ready(function() {
@@ -276,5 +302,24 @@ cardVideoHandler();;
 		});
 	}
 };
+{
+  let gallerySlider = document.querySelector('.gallery-slider');
+  if(gallerySlider) {
+    var galleryThumbs = new Swiper(gallerySlider.querySelector('.gallery-slider__thumbs'), {
+      spaceBetween: 10,
+      slidesPerView: 'auto',
+      freeMode: true,
+      watchSlidesVisibility: true,
+      watchSlidesProgress: true,
+    });
+    var galleryTop = new Swiper(gallerySlider.querySelector('.gallery-slider__top'), {
+      spaceBetween: 0,
+      thumbs: {
+        swiper: galleryThumbs
+      }
+    });
+  }
+}
+;
 
 });
